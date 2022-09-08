86d2e107a31a7c69f8bc6ff2a278e239f4aeaffc
RomanKhrapchun <khrapchun@gmail.com>
2022-09-08 15:58:28 +0300

    Revorced 9(if)

--- src/Task_9/Task_9.java
+++ src/Task_9/Task_9.java
@@ -6,19 +6,18 @@ public class Task_9 {
     public static void main(final String[] args) {
         final CustomDouble a = new CustomDouble(0.5);
         final CustomDouble b = new CustomDouble(0.3);
-        final int inputSign = 3;
-        switch (inputSign) {
-            case 1:
-                System.out.println(a.sum(b));
-                break;
-            case 2:
-                System.out.println(a.subtraction(b));
-                break;
-            case 3:
-                System.out.println(Calculator.multiply(a, b));
-                break;
-            case 4:
-                System.out.println(Calculator.divide(a, b));
+        final char operator = '*';
+        if(operator == '+') {
+            System.out.println(a.sum(b));
+        }
+        if(operator == '-') {
+            System.out.println(a.subtraction(b));
+        }
+        if(operator == '*') {
+            System.out.println(Calculator.multiply(a, b));
+        }
+        if(operator == '/') {
+            System.out.println(Calculator.divide(a, b));
         }
     }
 }
